56e2cfb920cb7f9284799f2eb5c0eac8fd567575
SteveNguyen <nam.ngh2211@mail.com>
2023-08-10 10:54:44 +0700

    refactor

--- Assets/DragAndDrop.cs
+++ Assets/DragAndDrop.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
+    [SerializeField] GameObject particleVFX;
+
     private bool _dragging;
 
     private Vector2 _offset;
 
     public static bool mouseButtonReleased;
+
+    private float min_X = -2.34f;
+
+    private float max_X = 2.34f;
+
+    private float min_Y = -4f;
+
+    private float max_Y = 5f;
 
     private void OnMouseDown()
     {
@@ -24,11 +34,65 @@ public class DragAndDrop : MonoBehaviour
         var mousePosition = GetMousePos();
 
         transform.position = mousePosition - _offset;
+
+
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void Update()
     {
-        _dragging = false;
+        if (transform.position.x < min_X)
+        {
+            Vector3 moveDirX = new Vector3(min_X, transform.position.y, 0f);
+            transform.position = moveDirX;
+        }
+        else if (transform.position.x > max_X)
+        {
+            Vector3 moveDirX = new Vector3(max_X, transform.position.y, 0f);
+            transform.position = moveDirX;
+        }
+        else if (transform.position.y < min_Y)
+        {
+            Vector3 moveDirX = new Vector3(transform.position.x, min_Y, 0f);
+            transform.position = moveDirX;
+        }
+        else if (transform.position.y > max_Y)
+        {
+            Vector3 moveDirX = new Vector3(transform.position.x, max_Y, 0f);
+            transform.position = moveDirX;
+        }
+        else if (transform.position.x < min_X && transform.position.y < min_Y)
+        {
+            Vector3 moveDirX = new Vector3(min_X, min_Y, 0f);
+            transform.position = moveDirX;
+        }
+        else if (transform.position.x < min_X && transform.position.y > max_Y)
+        {
+            Vector3 moveDirX = new Vector3(min_X, max_Y, 0f);
+            transform.position = moveDirX;
+        }
+        else if (transform.position.x > max_X && transform.position.y > max_Y)
+        {
+            Vector3 moveDirX = new Vector3(max_X, max_Y, 0f);
+            transform.position = moveDirX;
+        }
+        else if (transform.position.x > max_X && transform.position.y < min_Y)
+        {
+            Vector3 moveDirX = new Vector3(max_X, min_Y, 0f);
+            transform.position = moveDirX;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (gameObject.tag == collision.gameObject.tag)
+        {
+            GameManager.Instance.levels[GameManager.Instance.GetCurrentIndex()].items.Remove(this);
+            GameManager.Instance.levels[GameManager.Instance.GetCurrentIndex()].items.Remove(collision.gameObject.GetComponent<DragAndDrop>());
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+            GameObject explosion = Instantiate(particleVFX, transform.position, transform.rotation);
+            Destroy(explosion, .75f);
+        }
     }
 
     private void OnMouseUp()
@@ -40,4 +104,5 @@ public class DragAndDrop : MonoBehaviour
     {
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
+
 }
